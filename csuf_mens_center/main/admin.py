6061d72b7434787ff6c155aaa84e6af7a1350d2e
Dahlia Sukaik <dahliasukaik@csu.fullerton.edu>
2024-07-10 23:42:55 -0700

    added models to the admin.py now admin can see the students added

--- csuf_mens_center/main/admin.py
+++ csuf_mens_center/main/admin.py
@@ -1,3 +1,24 @@
+# main/admin.py
 from django.contrib import admin
+from .models import Student, Event, Attendance, Alumni
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone')
+    search_fields = ('first_name', 'last_name', 'email')
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('event_name', 'event_date')
+    search_fields = ('event_name',)
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('student', 'event', 'attendance_date', 'status')
+    search_fields = ('student__first_name', 'student__last_name', 'event__event_name')
+
+@admin.register(Alumni)
+class AlumniAdmin(admin.ModelAdmin):
+    list_display = ('student', 'graduation_date')
+    search_fields = ('student__first_name', 'student__last_name')
+
